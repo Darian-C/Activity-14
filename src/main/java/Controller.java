44f8d3ae7d4928bc8874980007b453bcc150a4ae
Darian-C <Dcolon8953@eagle.fgcu.edu>
2019-12-04 18:39:13 -0500

    Initial commit

---
+++ src/main/java/Controller.java
@@ -0,0 +1,47 @@
+import javafx.scene.control.TextArea;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Controller {
+    public TextArea taOutput;
+
+    public void initialize(){
+    final String JDBC_DRIVER = "org.h2.Driver";
+    final String DB_URL = "jdbc:h2:./res/HR";
+
+    //  Database credentials
+    final String USER = "";
+    final String PASS = "";
+    Connection conn = null;
+    Statement stmt = null;
+
+    try {
+        // STEP 1: Register JDBC driver
+        Class.forName(JDBC_DRIVER);
+
+        //STEP 2: Open a connection
+        conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+        //STEP 3: Execute a query
+        stmt = conn.createStatement();
+
+        String sql = "SELECT * FROM JOBS";
+
+        ResultSet rs = stmt.executeQuery(sql);
+        while (rs.next()) {
+            taOutput.appendText(rs.getString(1)+"\n");
+        }
+
+        // STEP 4: Clean-up environment
+        stmt.close();
+        conn.close();
+    } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+}}
